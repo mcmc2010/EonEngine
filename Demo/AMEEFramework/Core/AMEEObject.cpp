693c9fbeebbd74bb300ceb8e17da353afe0befee
unreal2012 <magicskyapple@gmail.com>
2026-07-04 19:28:17 +0800

    refactor: Object uses IDGenerator instead of local counter
    - Remove static ms_NextID from Object
    - Use IDGenerator::Generate(IDType::Node) for ID
    - Add GetIDType() helper method

--- Demo/AMEEFramework/Core/AMEEObject.cpp
+++ Demo/AMEEFramework/Core/AMEEObject.cpp
@@ -2,10 +2,8 @@
 
 namespace AMEE {
 
-uint64_t Object::ms_NextID = 1;
-
 Object::Object()
-    : m_ID(ms_NextID++)
+    : m_ID(IDGenerator::Generate(IDType::Node))
 {
 }
 
--- Demo/AMEEFramework/Core/AMEEObject.hpp
+++ Demo/AMEEFramework/Core/AMEEObject.hpp
@@ -1,6 +1,7 @@
 #ifndef __AMEE_OBJECT_H__
 #define __AMEE_OBJECT_H__
 #pragma once
+#include "AMEEGenerateUtils.hpp"
 #include <cstdint>
 #include <string>
 
@@ -12,13 +13,14 @@ public:
     virtual ~Object() = default;
 
     uint64_t GetID() const { return m_ID; }
+    IDType GetIDType() const { return IDGenerator::GetType(m_ID); }
+
     const std::string& GetName() const { return m_Name; }
     void SetName(const std::string& Name) { m_Name = Name; }
 
 private:
     uint64_t m_ID;
     std::string m_Name;
-    static uint64_t ms_NextID;
 };
 
 } // namespace AMEE
